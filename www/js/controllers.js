8d4ed3269bb13f26c86c0e6068bb2c75058c85da
Rasathi <rasathi03@gmail.com>
2015-11-20 18:37:47 +0530

    Including content option for the app

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,15 +1,19 @@
+
 angular.module('controllers', [])
 
 .controller('HomeCtrl', [
   '$scope',
   function($scope) {
-    
+
   },
   ])
 .controller('AccountCtrl', [
   '$scope',
   function($scope) {
-      
+
   },
   ]);
-    
+  angular.module('starter.controllers', [])
+    .controller('ToDoListCtrl',
+    function ($scope) {
+});
