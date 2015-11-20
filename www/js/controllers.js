69cc6e5688867a5f4c419a46db8c4400e228fc01
Rasathi <rasathi03@gmail.com>
2015-11-20 19:22:52 +0530

    three changes

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,19 +1,41 @@
+angular.module('ToDo.controllers', [])
 
-angular.module('controllers', [])
+.controller('ToDoListCtrl', function ($scope, $ionicModal) {
 
-.controller('HomeCtrl', [
-  '$scope',
-  function($scope) {
+// array list which will contain the items added
+$scope.toDoListItems = [];
 
-  },
-  ])
-.controller('AccountCtrl', [
-  '$scope',
-  function($scope) {
+//init the modal
+$ionicModal.fromTemplateUrl('modal.html', {
+  scope: $scope,
+  animation: 'slide-in-up'
+}).then(function (modal) {
+  $scope.modal = modal;
+});
 
-  },
-  ]);
-  angular.module('starter.controllers', [])
-    .controller('ToDoListCtrl',
-    function ($scope) {
+// function to open the modal
+$scope.openModal = function () {
+  $scope.modal.show();
+};
+
+// function to close the modal
+$scope.closeModal = function () {
+  $scope.modal.hide();
+};
+
+//Cleanup the modal when we're done with it!
+$scope.$on('$destroy', function () {
+  $scope.modal.remove();
+});
+
+//function to add items to the existing list
+$scope.AddItem = function (data) {
+  $scope.toDoListItems.push({
+    task: data.newItem,
+    status: 'not done'
+  });
+  data.newItem = '';
+  $scope.closeModal();
+};
+
 });
